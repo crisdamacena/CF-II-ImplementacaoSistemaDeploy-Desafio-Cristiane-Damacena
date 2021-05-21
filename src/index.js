ff8733fa62703af6ed69112800d7f8f503cc1f91
Cristiane Damacena <damacena.cris@gmail.com>
2021-05-21 02:58:36 -0300

    Página html

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello World do Nosso tutorial');
+  //res.status(200).send('Hello World do Nosso tutorial');
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 module.exports = app;
